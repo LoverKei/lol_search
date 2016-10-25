35d613394a49ec490a35e8f8e7eb529904d039b0
LoverKei <loverkeii@gmail.com>
2016-10-25 02:05:04 +0000

    [lol_search] add log to file & stdout. Modify api dir_name  - added log to file & stdout steram  - moved api test result to api_result  - modify api dir_name (lol_test --> lol_api)

--- lol_api/apikey.py
+++ lol_api/apikey.py
@@ -1,6 +1,6 @@
 # api_key.py
 
-api_key = "insert your api key"
+api_key = "7212059b-d947-42ff-9690-944d264d44ce"
 
 def getKey():
 	return api_key
--- telegram_lol_search/myToken.py
+++ telegram_lol_search/myToken.py
@@ -1,4 +1,4 @@
-TOKEN = "insert Your TOKEN"
+TOKEN = "250283930:AAG8256NzmBu3lAMbs12HlyjpdYdeETxb_E"
 
 def getToken():
 	return TOKEN
